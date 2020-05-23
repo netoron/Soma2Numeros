ab3ab8bcbe521762cf08ec46c1b8e0c9a8a34029
Erick. santos <erickblz@outlook.com>
2020-05-22 21:44:19 -0300

    Adicionando o programa

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Soma2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string primeiro;
+            string segundo;
+            double soma1;
+            double soma2;
+
+            double item;
+            
+            Console.WriteLine("Soma de dois numeros");
+            Console.Write("Digite o Primeiro número:");
+            
+            primeiro = Console.ReadLine();
+            
+            Console.Write("Digite o Segundo número:");
+            segundo = Console.ReadLine();
+
+            soma1 = Convert.ToDouble(primeiro);
+            soma2 = Convert.ToDouble(segundo);
+
+            item = soma1 * soma2;
+            Console.WriteLine($"A Soma de {primeiro} e de {segundo} é {item}");
+
+
+        }
+    }
+}
